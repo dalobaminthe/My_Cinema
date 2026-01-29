18fed9654c23efead6294f3f57faec3ba41ad1cc
daloba-epitech <daloba.minthe@epitech.eu>
2026-01-29 11:41:06 +0100

    test pdo FAIT

--- backend/repositories/MovieRepository.php
+++ backend/repositories/MovieRepository.php
@@ -1,3 +1,13 @@
 <?php
 //les repositories sont les SEULS à parler à la base de données. Chaque table = 1 repository.
+
+require_once __DIR__ . '/../config/database.php';
+
+// test $pdo
+$stmt = $pdo->query("SELECT * FROM movies");
+$movies = $stmt->fetchAll();
+
+print_r($movies);
+
+echo "Connexion réussie PDO";
 ?>
